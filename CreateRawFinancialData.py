cd56199e46deb3d0f1b4cc7802fc244f2feba653
yusugiura <19901220dev@gmail.com>
2021-05-30 11:59:54 +0900

    fix: driname

--- CreateRawFinancialData.py
+++ CreateRawFinancialData.py
@@ -1,17 +1,17 @@
 import tabula
 from colored import fg, bg, attr
-import glob
+import glob, os
 
 class CreateRawFinancialData:
 
-    PDF_DIR = 'yufo/'
+    PDF_DIR = 'yufo/financial/'
     CSV_DIR = 'csv/'
     PDF_EXTENTION = '.pdf'
     CSV_EXTENTION = '.csv'
 
     def execute(self) -> list:
         # 処理準備
-        companyName = CreateRawFinancialData.inputCompanyName()
+        companyName = CreateRawFinancialData.inputCompanyName(self)
         # 処理実行
         print('%s%s 処理を開始します %s' % (fg('black'), bg('grey_93'), attr('reset')))
         financialPdfList = CreateRawFinancialData.importPdf(self)
@@ -21,12 +21,20 @@ class CreateRawFinancialData:
 
         print('%s%s 処理を完了しました %s' % (fg('black'), bg('green'), attr('reset')))
 
-    def inputCompanyName() -> str:
+    def inputCompanyName(self) -> str:
         print('企業名or証券コードを入力してください')
         companyName = input('> ')
+
         if companyName == '':
-            return 'undefined_'
-        return companyName + '_'
+            companyName =  'undefined'
+        
+        # csv格納ディレクトリの生成
+        dir = self.CSV_DIR + companyName + '/'
+        print(dir)
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        
+        return companyName
  
     def importPdf(self) -> list:
         pdfList = glob.glob(self.PDF_DIR + '*.pdf')
@@ -46,7 +54,7 @@ class CreateRawFinancialData:
     def createFinancialData(self, financialPdfList: list, companyName: str) -> list:
         for pdf in financialPdfList:
             financialDataObject = CreateRawFinancialData.readPdf(self, pdf)
-            CreateRawFinancialData.createCsv(self, financialDataObject, companyName + str(pdf['closingYear']))
+            CreateRawFinancialData.createCsv(self, financialDataObject, companyName, pdf['closingYear'])
             
     def readPdf(self, financialPdf: object) -> object:
         print(financialPdf['closingYear'] + ': 読み込みを開始します')
@@ -57,9 +65,10 @@ class CreateRawFinancialData:
         except:
             print('%s%s pdfの読み込みに失敗しました。ページ指定を見直してください %s' % (fg('white'), bg('red'), attr('reset')))
 
-    def createCsv(self, financialDataObject: object, fileName: str) -> None:
+    def createCsv(self, financialDataObject: object, dirName: str, fileName: str) -> None:
         try:
-            financialDataObject.to_csv(self.CSV_DIR + fileName + self.CSV_EXTENTION, index=None)
+            dir = self.CSV_DIR + '/' + dirName
+            financialDataObject.to_csv(dir + '/' + fileName + self.CSV_EXTENTION, index=None)
             print(fileName + ': csvの作成を完了しました')
         except:
             print('%s%s csvの作成に失敗しました。ページ指定を見直してください %s' % (fg('white'), bg('red'), attr('reset')))
